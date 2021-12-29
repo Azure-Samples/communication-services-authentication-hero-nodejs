c3e3e1592e5c5198dd2303e3dcdbda5856926295
Clarence Li <clarenceli@microsoft.com>
2021-12-29 10:57:31 -0800

    Wrap the OBO workflow in the aadServices

--- src/services/aadService.ts
+++ src/services/aadService.ts
@@ -9,7 +9,7 @@ import { Configuration, ConfidentialClientApplication } from '@azure/msal-node';
 import { appSettings } from '../appSettings';
 import { Constants } from '../config/constants';
 
-export const aadManager = {
+export const aadService = {
   /**
    * Create a confidential client application that lets users utilize the OBO workflow
    */
@@ -25,5 +25,48 @@ export const aadManager = {
     const confidentialClientApplication = new ConfidentialClientApplication(msalConfig);
 
     return confidentialClientApplication;
+  },
+
+  /** 
+   * Generate the AAD token (i.e. token A in OBO workflow)
+   */
+  createAADToken: async (confidentialClientApplication: ConfidentialClientApplication): Promise<string> => {
+    const authCodeUrlParameters = {
+      scopes: [appSettings.remoteResources.azureActiveDirectory.appRegistrations.webAPIScope],
+      redirectUri: appSettings.remoteResources.azureActiveDirectory.appRegistrations.redirectUri
+    };
+
+    // Get url to sign user in and consent to scopes needed for application
+    const authCode = await confidentialClientApplication.getAuthCodeUrl(authCodeUrlParameters);
+
+    // Generate the AAD token 
+    const tokenRequest = {
+      code: authCode,
+      scopes: [appSettings.remoteResources.azureActiveDirectory.appRegistrations.webAPIScope],
+      redirectUri: appSettings.remoteResources.azureActiveDirectory.appRegistrations.redirectUri
+    };
+
+    const aadTokenResponse = await confidentialClientApplication.acquireTokenByCode(tokenRequest);
+    
+    return aadTokenResponse.accessToken;
+  }, 
+
+  /**
+   * Generate the OBO token (i.e. token B in OBO workflow)
+   */
+  createOBOToken: async(): Promise<string> => {
+    const confidentialClientApplication = aadService.createConfidentialClientApplication();
+    const aadToken = await aadService.createAADToken(confidentialClientApplication);
+    
+    // Generate the OBO token
+    const oboRequest = {
+      oboAssertion: aadToken,
+      scopes: ['user.read']
+    };
+
+    const oboTokenResponse = await confidentialClientApplication.acquireTokenOnBehalfOf(oboRequest);
+    
+    return oboTokenResponse.accessToken;
   }
+
 };
