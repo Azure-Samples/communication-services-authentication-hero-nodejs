684bdf4be669bbcbd518810922a026e81d5237ea
Aline Caillaud <alcail@microsoft.com>
2022-01-10 12:35:09 -0800

    fix root folder for tests

--- jest.config.js
+++ jest.config.js
@@ -111,7 +111,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: './src',
+  rootDir: './tests',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: ['src'],
