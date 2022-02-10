d3eb7fe47cf06db3a5a9d3123c2cc7c9bb5e3a19
Clarence Li <clarenceli@microsoft.com>
2022-02-10 09:45:21 -0800

    Add validation to verify AAD token's expiration time

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,8 +5,10 @@
 
 import { NextFunction, Request, Response } from 'express';
 import { ErrorResponse } from '../types/errorResponse';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
 
 const GET_AUTHORIZATION_CODE_ERROR = 'Fail to get the authorization code from the request header';
+const AAD_EXPIRATION_ERROR = 'The AAD token provided is already expired';
 
 // Get an AAD token passed through request header
 export const getAADTokenViaRequest = (req: Request): string => {
@@ -29,5 +31,12 @@ export const validateAuthorizedHeader = (req: Request, res: Response, next: Next
   if (!authHeader || !authHeader.split(' ')[1]) {
     return res.status(401).json(createErrorResponse(401, GET_AUTHORIZATION_CODE_ERROR));
   }
+
+  // Verify if the AAD token is expired
+  const decodedToken: JwtPayload = jwtDecode(authHeader.split(' ')[1]);
+  if (Date.now() >= decodedToken.exp * 1000) {
+    return res.status(401).json(createErrorResponse(401, AAD_EXPIRATION_ERROR));
+  }
+
   next();
 };
